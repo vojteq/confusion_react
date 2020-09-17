12cfd11dd79330e6a2eded5869f86e7248f3966d
vojteq <vojteq17@gmail.com>
2020-09-17 15:15:37 +0200

    Redux Thunk

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,7 +1,20 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionTypes from '../redux/ActionTypes';
 
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (state = {
+    isLoading: true,
+    errMess: null,
+    dishes: []
+}, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_DISHES:
+            return { ...state, isLoading: false, errMess: null, dishes: action.paylode };
+
+        case ActionTypes.DISHES_LOADING:
+            return { ...state, isLoading: true, errMess: null, dishes: [] };
+
+        case ActionTypes.DISHES_FAILED:
+            return { ...state, isLoading: false, errMess: action.paylode, dishes: [] };
+
         default:
             return state;
     }
